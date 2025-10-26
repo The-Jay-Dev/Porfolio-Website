08f728cd13c505dbfc139c3c656518b05cb86475
Emissary-Dev <jayemissary@gmail.com>
2025-10-26 08:28:57 -0400

    Fixed broken links

--- src/project_rebirth/components/DownloadButton.jsx
+++ src/project_rebirth/components/DownloadButton.jsx
@@ -4,7 +4,7 @@ const DownloadButton = ({ btnText, btnSubText }) => {
   return (
     <div>
       <a
-        href="https://github.com/Emissary-Dev/Rebirth"
+        href="https://github.com/The-Jay-Dev/Rebirth"
         className="scale-90 sm:scale-105 md:scale-120 lg:scale-100 text-[.8rem] lg:!text-[1rem] xl:!text-[1.3rem] py-7 px-6 inline-block relative rebirth-cta-btn hover:bg-white hover:text-black bg-black/20 transition text-white border-white border-primary border-2 z-3 "
       >
         {btnText}
